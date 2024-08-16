e11496d50453e6c5a22207d6de736f4d159ceee7
LucaElisio <personal.elix@gmail.com>
2024-08-16 18:18:44 +0200

    feat: added other two routes in router.js

--- src/router.js
+++ src/router.js
@@ -1,12 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import HomePage from "./pages/HomePage.vue";
+import DaysPage from "./pages/DaysPage.vue";
+import BagPage from "./pages/BagPage.vue";
+
+
 export const router = createRouter ({
     history: createWebHistory(),
-    router: [
+    routes: [
         {
             path: "/",
-            name: home,
+            name: "Home",
             component: HomePage
+        },
+        {
+            path: "/days",
+            name: "Giornate",
+            component: DaysPage
+        },
+        {
+            path: "/bag",
+            name: "Borsa",
+            component: BagPage
         }
     ]
 })
+
