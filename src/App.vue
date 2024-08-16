67f8fcdb72fcf31c6a57c4ca326e56e87fc519e7
LucaElisio <personal.elix@gmail.com>
2024-08-16 16:20:38 +0200

    feat: import appheader component to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 
 <script>
 import AppMain from "./components/AppMain.vue";
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
   components: {
     AppMain,
+    AppHeader
   }
 }
 
 </script>
 
 <template>
+
+  <AppHeader />
+
   <AppMain />
+  
 </template>
 
 <style scoped lang="scss">
