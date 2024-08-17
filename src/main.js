ebbc47feea208d314b44e54ad4bf4028e7e59df7
LucaElisio <personal.elix@gmail.com>
2024-08-17 17:52:58 +0200

    feat: add insert new day button with only day and date

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import './style/general.scss'
 import App from './App.vue'
 import { router } from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
 
 createApp(App).use(router).mount('#app')
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,33 +1,56 @@
 <script>
-import {store} from "../store.js";
+import { store } from "../store.js";
 
 export default {
-    data(){
-        return{
-            store
+    data() {
+        return {
+            store,
+            giornoInserito: 0,
+            dataInserita: "",
         }
     },
 
     methods: {
 
-        changeStatus: function(giorno){
+        changeStatus: function (giorno) {
 
-        // Trovo il giorno da aggiornare
-        const dayToChange = store.viaggio.find(g => g.giorno === giorno)
-        
-        // Cambio valore dell'item
-        dayToChange.completato = !dayToChange.completato
+            // Trovo il giorno da aggiornare
+            const dayToChange = store.viaggio.find(g => g.giorno === giorno)
 
-        //Trasformo in json l'array viaggio
-        const jsonViaggio = JSON.stringify(store.viaggio);
-        
-        //Salvo nel localstorage il file json
-        localStorage.setItem("viaggio", jsonViaggio)
+            // Cambio valore dell'item
+            dayToChange.completato = !dayToChange.completato
+
+            //Trasformo in json l'array viaggio
+            const jsonViaggio = JSON.stringify(store.viaggio);
+
+            //Salvo nel localstorage il file json
+            localStorage.setItem("viaggio", jsonViaggio)
 
         },
 
-        deleteDay: function(giorno){
-            store.viaggio.splice(giorno-1, 1)
+        deleteDay: function (giorno) {
+
+            console.log(store.viaggio);
+
+            console.log(giorno);
+            // Trovo indice del giorno selezionato
+            let index = store.viaggio.findIndex(g => g.giorno === giorno);
+
+            console.log(index);
+
+            store.viaggio.splice(index, 1)
+            
+
+            // Trasformo in json e lo salvo nel localstorage
+            const jsonViaggio = JSON.stringify(store.viaggio);
+            localStorage.setItem("viaggio", jsonViaggio)
+        },
+
+        saveNewDay: function () {
+
+            const newDay = { giorno: this.giornoInserito, data: this.dataInserita };
+
+            store.viaggio.push(newDay);
 
             // Trasformo in json e lo salvo nel localstorage
             const jsonViaggio = JSON.stringify(store.viaggio);
@@ -36,7 +59,7 @@ export default {
 
     },
 
-    created(){
+    created() {
         const localTrip = localStorage.getItem("viaggio");
         if (localTrip !== null) {
             store.viaggio = JSON.parse(localTrip);
@@ -66,20 +89,51 @@ export default {
             </thead>
             <tbody>
                 <tr v-for="item in store.viaggio">
-                    <th scope="row" class="text-center">{{item.giorno}}</th>
-                    <td class="text-center">{{item.data}}</td>
-                    <td class="text-center">{{item.tappe.length}}</td>
+                    <th scope="row" class="text-center">{{ item.giorno }}</th>
+                    <td class="text-center">{{ item.data }}</td>
+                    <td class="text-center">{{ item.tappe ? item.tappe.length : "Non definito" }}</td>
                     <td class="text-center">
                         <button class="btn" @click="changeStatus(item.giorno)">
-                        <i v-if="item.completato" class="fa-solid fa-circle-check" style="color: green;"></i>
-                        <i v-else class="fa-solid fa-circle-xmark" style="color: red;"></i>
+                            <i v-if="item.completato" class="fa-solid fa-circle-check" style="color: green;"></i>
+                            <i v-else class="fa-solid fa-circle-xmark" style="color: red;"></i>
                         </button>
                     </td>
-                    <td class="text-center"><router-link :to="{name: 'Giornate'}" class="btn btn-outline-warning">Dettagli</router-link></td>
-                    <td><button @click="deleteDay(item.giorno)" class="btn btn-outline-danger"><i class="fa-solid fa-trash"></i></button></td>
+                    <td class="text-center"><router-link :to="{ name: 'Giornate' }"
+                            class="btn btn-outline-warning">Dettagli</router-link></td>
+                    <td><button @click="deleteDay(item.giorno)" class="btn btn-outline-danger"><i
+                                class="fa-solid fa-trash"></i></button></td>
                 </tr>
             </tbody>
         </table>
+
+        <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#insertDay">Inserisci
+            Giorno</button>
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="insertDay" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Inserisci Giorno</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="GET"></form>
+                        <label for="giorno">Inserisci Giorno</label><br>
+                        <input type="number" name="giorno" id="giorno" v-model="giornoInserito"><br>
+                        <label for="data">Inserisci Data</label><br>
+                        <input type="text" name="data" id="data" v-model="dataInserita">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                        <button type="button" class="btn btn-primary"  @click="saveNewDay" data-bs-dismiss="modal">Salva</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 
 </template>
