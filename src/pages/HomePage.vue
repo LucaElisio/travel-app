1f57373e43ef6c55bd0647d5027cc13da834e041
LucaElisio <personal.elix@gmail.com>
2024-08-17 14:38:48 +0200

    feat: homepage table with changeStatus function and storage system

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,6 +6,33 @@ export default {
         return{
             store
         }
+    },
+
+    methods: {
+        changeStatus: function(giorno){
+
+        // Trovo il giorno da aggiornare
+        const dayToChange = store.viaggio.find(g => g.giorno === giorno)
+        
+        // Cambio valore dell'item
+        dayToChange.completato = !dayToChange.completato
+
+        //Trasformo in json l'array viaggio
+        const jsonViaggio = JSON.stringify(store.viaggio);
+        
+        //Salvo nel localstorage il file json
+        localStorage.setItem("viaggio", jsonViaggio)
+
+        }
+    },
+
+    created(){
+        const localTrip = localStorage.getItem("viaggio");
+        if (localTrip !== null) {
+            store.viaggio = JSON.parse(localTrip);
+        } else {
+            store.viaggio = [];
+        }
     }
 }
 </script>
@@ -18,18 +45,23 @@ export default {
         <table class="table">
             <thead>
                 <tr>
-                    <th scope="col">Giorno</th>
-                    <th scope="col">Data</th>
-                    <th scope="col">N. Tappe</th>
-                    <th scope="col">Completato</th>
+                    <th scope="col" class="text-center">Giorno</th>
+                    <th scope="col" class="text-center">Data</th>
+                    <th scope="col" class="text-center">N. Tappe</th>
+                    <th scope="col" class="text-center">Completato</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="item in store.viaggio">
-                    <th scope="row">{{item.giorno}}</th>
-                    <td>{{item.data}}</td>
-                    <td>{{item.tappe.length}}</td>
-                    <td>{{item.completato}}</td>
+                    <th scope="row" class="text-center">{{item.giorno}}</th>
+                    <td class="text-center">{{item.data}}</td>
+                    <td class="text-center">{{item.tappe.length}}</td>
+                    <td class="text-center">
+                        <button class="btn" @click="changeStatus(item.giorno)">
+                        <i v-if="item.completato" class="fa-solid fa-circle-check" style="color: green;"></i>
+                        <i v-else class="fa-solid fa-circle-xmark" style="color: red;"></i>
+                        </button>
+                    </td>
                 </tr>
             </tbody>
         </table>
