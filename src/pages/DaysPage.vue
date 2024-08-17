aa65013ef2a149f553b33c9b5090bdc02c19fa68
LucaElisio <personal.elix@gmail.com>
2024-08-17 16:26:27 +0200

    feat: check buttons

--- src/pages/DaysPage.vue
+++ src/pages/DaysPage.vue
@@ -6,6 +6,33 @@ export default {
         return {
             store
         }
+    },
+
+    created() {
+        const localTrip = localStorage.getItem("viaggio");
+        if (localTrip !== null) {
+            store.viaggio = JSON.parse(localTrip);
+        } else {
+            store.viaggio = [];
+        }
+    },
+
+    methods: {
+        changeStatus: function (giorno) {
+
+            // Trovo il giorno da aggiornare
+            const dayToChange = store.viaggio.find(g => g.giorno === giorno)
+
+            // Cambio valore dell'item
+            dayToChange.completato = !dayToChange.completato
+
+            //Trasformo in json l'array viaggio
+            const jsonViaggio = JSON.stringify(store.viaggio);
+
+            //Salvo nel localstorage il file json
+            localStorage.setItem("viaggio", jsonViaggio)
+
+        }
     }
 }
 </script>
@@ -16,7 +43,14 @@ export default {
 
     <div class="container" v-for="item in store.viaggio">
 
-        <h3>Giorno {{ item.giorno }}</h3>
+        <div class="d-flex">
+            <h3>Giorno {{ item.giorno }}</h3>
+            <button class="btn" @click="changeStatus(item.giorno)">
+                <i v-if="item.completato" class="fa-solid fa-circle-check" style="color: green;"></i>
+                <i v-else class="fa-solid fa-circle-xmark" style="color: red;"></i>
+            </button>
+        </div>
+
         <h6>Data: {{ item.data }}</h6>
 
         <ul>
