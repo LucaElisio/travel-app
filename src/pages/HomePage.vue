10c0c4736eea00d503c4a9ef7f96af4e535af2e0
LucaElisio <personal.elix@gmail.com>
2024-08-17 09:33:21 +0200

    feat: print a simple table of the trip

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,40 @@
 <script>
+import {store} from "../store.js";
+
+export default {
+    data(){
+        return{
+            store
+        }
+    }
+}
 </script>
 
 <template>
-    <h1>HomePage</h1>
+
+    <div class="container">
+        <h1 class="text-center">Il tuo viaggio</h1>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Giorno</th>
+                    <th scope="col">Data</th>
+                    <th scope="col">N. Tappe</th>
+                    <th scope="col">Completato</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in store.viaggio">
+                    <th scope="row">{{item.giorno}}</th>
+                    <td>{{item.data}}</td>
+                    <td>{{item.tappe.length}}</td>
+                    <td>{{item.completato}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
 </template>
 
-<style>
-</style>
+<style scoped lang="scss"></style>
