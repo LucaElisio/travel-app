80956b7e36995fa3da91743d4d8b85756c0b7296
LucaElisio <personal.elix@gmail.com>
2024-08-18 08:57:37 +0200

    feat: delete modal

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -30,16 +30,11 @@ export default {
 
         deleteDay: function (giorno) {
 
-            console.log(store.viaggio);
-
-            console.log(giorno);
             // Trovo indice del giorno selezionato
             let index = store.viaggio.findIndex(g => g.giorno === giorno);
 
-            console.log(index);
-
+            // Elimino elemento dall'array
             store.viaggio.splice(index, 1)
-            
 
             // Trasformo in json e lo salvo nel localstorage
             const jsonViaggio = JSON.stringify(store.viaggio);
@@ -88,21 +83,54 @@ export default {
                 </tr>
             </thead>
             <tbody>
+
                 <tr v-for="item in store.viaggio">
+
                     <th scope="row" class="text-center">{{ item.giorno }}</th>
+
                     <td class="text-center">{{ item.data }}</td>
+
                     <td class="text-center">{{ item.tappe ? item.tappe.length : "Non definito" }}</td>
+
                     <td class="text-center">
                         <button class="btn" @click="changeStatus(item.giorno)">
                             <i v-if="item.completato" class="fa-solid fa-circle-check" style="color: green;"></i>
                             <i v-else class="fa-solid fa-circle-xmark" style="color: red;"></i>
                         </button>
                     </td>
+
                     <td class="text-center"><router-link :to="{ name: 'Giornate' }"
                             class="btn btn-outline-warning">Dettagli</router-link></td>
-                    <td><button @click="deleteDay(item.giorno)" class="btn btn-outline-danger"><i
-                                class="fa-solid fa-trash"></i></button></td>
+
+                    <td>
+                        <button class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteDay">
+                            
+                            <i class="fa-solid fa-trash"></i>
+
+                            <!-- Delete Modal -->
+                            <div class="modal fade" id="deleteDay" tabindex="-1" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Sei sicuro?</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Annulla</button>
+                                            <button type="button" class="btn btn-danger" @click="deleteDay(item.giorno)"
+                                                data-bs-dismiss="modal">Elimina</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </button>
+                    </td>
+
                 </tr>
+
             </tbody>
         </table>
 
@@ -110,7 +138,7 @@ export default {
             Giorno</button>
 
 
-        <!-- Modal -->
+        <!-- Insert Modal -->
         <div class="modal fade" id="insertDay" tabindex="-1" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -127,11 +155,14 @@ export default {
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                        <button type="button" class="btn btn-primary"  @click="saveNewDay" data-bs-dismiss="modal">Salva</button>
+                        <button type="button" class="btn btn-primary" @click="saveNewDay"
+                            data-bs-dismiss="modal">Salva</button>
                     </div>
                 </div>
             </div>
         </div>
+
+
 
 
     </div>
